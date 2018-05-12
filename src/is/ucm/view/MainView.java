77e8a3947bc7e784ba21137b7cf5f206a7be4be6
diegoambite <diegoambite@gmail.com>
2018-05-12 15:20:53 +0200

    icons and some swim

--- src/is/ucm/view/MainView.java
+++ src/is/ucm/view/MainView.java
@@ -1,7 +1,34 @@
 package is.ucm.view;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MainView extends JFrame{
+import es.ucm.fdi.control.Controller;
+import es.ucm.fdi.view.GuiViewObserver;
+import is.ucm.controller.Controler;
 
+public class MainView extends JFrame{
+	
+	private Toolbar _toolbar;
+	
+	private FridgeTableView _fridge;
+	private ToBuyTableView _tobuy;
+	
+	private Controler _controller;
+	
+	public MainView(Controler ctrl) {
+		super("Traffic Simulator");
+		_controller = ctrl;
+		//_obs = new ArrayList<GuiViewObserver>();
+		initGUI();
+	}
+	
+	
+	public void initGUI() {
+		
+		
+	}
+	
 }
---
+++ src/is/ucm/view/Toolbar.java
@@ -0,0 +1,53 @@
+package is.ucm.view;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+
+
+public class Toolbar extends JToolBar{
+	
+	private JButton _fridgeView, _buyView, _close;
+
+	public Toolbar(){//habra que meterle el controler
+		super();	
+
+		//crtl.addObserver(this);
+	
+		_fridgeView = new JButton();
+		_fridgeView.setActionCommand("fridge");
+		_fridgeView.setToolTipText("Open fridge list");
+		//_fridgeView.addActionListener((ActionListener) frame);
+
+		_fridgeView.setIcon(new ImageIcon(loadImage("resources/icons/fridge.png")));
+		this.add(_fridgeView);
+
+		_buyView = new JButton();
+		_buyView.setActionCommand("buy");
+		_buyView.setToolTipText("Open buy list");
+		//_buyView.addActionListener((ActionListener) frame);
+		_buyView.setIcon(new ImageIcon(loadImage("resources/icons/shopping-cart.png")));
+		this.add(_buyView);
+		
+		this.addSeparator();
+
+		_close = new JButton();
+		_close.setActionCommand("close");
+		_close.setToolTipText("Clear Text");
+		//_close.addActionListener((ActionListener) frame);
+		_close.setIcon(new ImageIcon(loadImage("resources/icons/close.png")));
+		this.add(_close);
+		
+		
+		
+	}
+	
+	private static Image loadImage(String path) {
+		return Toolkit.getDefaultToolkit().createImage(path);
+	}	
+}
