1f8de1ee0bb2cadba235c6e4e34139883a70cf33
diegoambite <diegoambite@gmail.com>
2018-05-12 13:36:28 +0200

    super class for food lists

--- src/is/ucm/model/Fridge.java
+++ src/is/ucm/model/Fridge.java
@@ -3,21 +3,7 @@ package is.ucm.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Fridge {
-	
-	List frigeList = new ArrayList();
-	boolean displayInventary;
-	
-	public void addNewItem(int id, int quantity, Category category){
-		
-		
-	}
-	
-	public void removeItem(int id, int quantity, Category category){
-		
-		
-	}
-	
+public class Fridge extends FoodContainer{
 	
 	
 	
