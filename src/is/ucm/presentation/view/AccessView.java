175ffca5ba9a2d15bd13604e4b7d8c3395be2d7f
gabma <gabma@Gabriele-XPS>
2018-05-20 19:28:53 +0200

    Modified login windows and so much more

--- src/is/ucm/presentation/view/AccessView.java
+++ src/is/ucm/presentation/view/AccessView.java
@@ -1,5 +1,6 @@
 package is.ucm.presentation.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -7,13 +8,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import is.ucm.exceptions.UserNotFoundException;
 import is.ucm.presentation.controller.Controller;
@@ -23,17 +28,17 @@ import is.ucm.presentation.controller.Controller;
  */
 @SuppressWarnings("serial")
 public class AccessView extends JFrame implements ActionListener {
-	
+
 	private Controller _ctr;
-	
-	 private JLabel l1, l2;
-	 private JTextField tf1;
-	 private JButton btn1, btn2;
-	 private JPasswordField p1;
-	 private JPanel _mainPanel;
-	 
-	 private JFrame _mainView;
-	 
+
+
+	private JTextField tf1;
+	private JButton btn1, btn2;
+	private JPasswordField p1;
+	private JPanel _mainPanel;
+
+	private JFrame _mainView;
+
 	public AccessView(Controller ctr, JFrame mainView) {
 		super("Register");
 		_ctr = ctr;
@@ -46,55 +51,53 @@ public class AccessView extends JFrame implements ActionListener {
 	 */
 	private void initGUI() {
 
-		_mainPanel = new JPanel(new GridLayout(6, 1));
+		_mainPanel = new JPanel(new GridLayout(4, 1));
 		this.setContentPane(_mainPanel);
+
+		Border b = BorderFactory.createLineBorder(Color.BLACK, 2);
+		tf1 = new JTextField("a");
+		p1 = new JPasswordField("a");
+
+		JScrollPane user = new JScrollPane(tf1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		user.setBorder(BorderFactory.createTitledBorder(b, "USERNAME"));
+		user.getViewport().setBackground(Color.WHITE);
 		
-		  l1 = new JLabel("Username");
-	      l2 = new JLabel("Password");
-	     
-	      tf1 = new JTextField("a");
-	      p1 = new JPasswordField("a");
-	     
-	      btn1 = new JButton("log in");
-	      btn2 = new JButton("register");
-	      
-	      btn1.setActionCommand("LOGIN");
-	      btn1.addActionListener(this);
-	      
-	      btn2.setActionCommand("REGISTER");
-	      btn2.addActionListener(this);
-	     	     
-	      l1.setBounds(80, 30, 400, 30);
-	      l2.setBounds(80, 70, 200, 30);
-	      
-	      tf1.setBounds(200, 30, 200, 30);
-	      p1.setBounds(200, 70, 200, 30);
-	      
-	      btn1.setBounds(150, 120, 100, 30);
-	      btn2.setBounds(150, 170, 100, 30);
-	       
-	      this.add(l1);
-	      
-	      this.add(tf1);
-	      
-	      this.add(l2);
-	      this.add(p1);
-	      
-	      this.add(btn1);
-	      this.add(btn2);
-	      
-	      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	      int height = (int) (screenSize.height * 0.60);
-	      int width = (int) (screenSize.width * 0.20);
-	      setPreferredSize(new Dimension(width, height));
-	      
-	      this.pack();
-	      this.setVisible(true);
+		JScrollPane passw = new JScrollPane(p1, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		passw.setBorder(BorderFactory.createTitledBorder(b, "PASSWORD"));
+		passw.getViewport().setBackground(Color.WHITE);
+
+		btn1 = new JButton("log in");
+		btn2 = new JButton("register");
+
+		btn1.setActionCommand("LOGIN");
+		btn1.addActionListener(this);
+
+		btn2.setActionCommand("REGISTER");
+		btn2.addActionListener(this);
+
+
+		btn1.setBounds(150, 120, 100, 30);
+		btn2.setBounds(150, 170, 100, 30);
+
+		this.add(user);
+		this.add(passw);
+
+		this.add(btn1);
+		this.add(btn2);
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = (int) (screenSize.height * 0.60);
+		int width = (int) (screenSize.width * 0.20);
+		setPreferredSize(new Dimension(350, 300));
+
+		this.pack();
+		this.setLocationRelativeTo(null); 
+		this.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		switch(e.getActionCommand()) {
 
 		case "SEND":
@@ -104,7 +107,7 @@ public class AccessView extends JFrame implements ActionListener {
 		case "REGISTER":
 			this._ctr.registerUser(tf1.getText(), new String(p1.getPassword()));
 			JOptionPane.showMessageDialog(_mainPanel, "Registered", "Your user is ready to use the Ifridge technology!", JOptionPane.INFORMATION_MESSAGE);
-			
+
 			_mainView.setVisible(true);
 			this.dispose();
 			break;
@@ -124,9 +127,9 @@ public class AccessView extends JFrame implements ActionListener {
 				JOptionPane.showMessageDialog(_mainPanel, "Error maaaan", e1.getMessage(), JOptionPane.ERROR_MESSAGE);
 			}
 			break;
-			
+
 		}
-		
+
 	}
 
 }
